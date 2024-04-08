079c5cc95221c931a3a1a9b25912a125dae52c43
Ryan Dempsey <rjd9896@gmail.com>
2024-04-09 02:00:20 +0900

    Updates

--- render-lists/src/List.jsx
+++ render-lists/src/List.jsx
@@ -1,24 +1,38 @@
+import PropTypes from 'prop-types'
 
-function List(){
-    const fruits = [{id: 1, name:"apple", calories: 95}, 
-                    {id: 2, name:"orange", calories: 45}, 
-                    {id: 3, name:"banana", calories: 105}, 
-                    {id: 4, name:"coconut", calories: 159}, 
-                    {id: 5, name:"pineapple", calories: 37}];
+function List(props){
 
+    const category = props.category;
+    const itemList = props.items;
+    
 // fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical
 // fruits.sort((a, b) => b.name.localeCompare(a.name)); // Reverse-Alphabetical
 // fruits.sort((a, b) => a.calories - b.calories); //Numeric
 // fruits.sort((a, b) => b.calories - a.calories); //Reverse-Numeric
 
     // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-       const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-                                           {fruit.name}: &nbsp;
-                                           <b>{fruit.calories}</b></li>);
+    const listItems = itemList.map(item => <li key={item.id}>
+                                           {item.name}: &nbsp;
+                                           <b>{item.calories}</b></li>);
 
-    return(<ul>{listItems}</ul>);
+    return(<>
+           <h3 className="list-category">{category}</h3>
+           <ol className="list-items">{listItems}</ol>
+           </>);
+}
+
+List.PropTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number,
+                                              name: PropTypes.string,
+                                              calories: PropTypes.number})),
+}
+
+List.defaultProps = {
+    category: "Category",
+    items: [],
 }
 
 export default List
