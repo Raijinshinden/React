63f9f102f94e2bdab76658a87373f852c0b6d445
Ryan Dempsey <rjd9896@gmail.com>
2024-04-10 01:39:25 +0900

    Update MyComponent.jsx

--- Update-Array-State copy/src/MyComponent.jsx
+++ Update-Array-State copy/src/MyComponent.jsx
@@ -2,29 +2,28 @@ import React, {useState} from "react";
 
 function MyComponent(){
 
-        const [car, setCar] = useState({year:2024, 
-                                       make:"Ford", 
-                                       model:"Mustang" });
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
-        function handleYearChange(event){
-            setCar(c => ({...c, year: event.target.value}));
-        }
+    function handleAddFood(){
 
-        function handleMakeChange(event){
-            setCar(c => ({...c, make: event.target.value}));
-        }
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
+        setFoods(f => [...f, newFood]);
+    }
 
-        function handleModelChange(event){
-            setCar(c => ({...c, model: event.target.value}));
-        }
+    function handleRemoveFood(index){
+        
+    }
 
-        return(<div>
-                    <p>Your favourite car is: {car.year} {car.make} {car.model}</p>
-
-                    <input type="number" value={car.year} onChange={handleYearChange}/><br/>
-                    <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
-                    <input type="text" value={car.model} onChange={handleModelChange}/><br/>
-               </div>)
-
+    return(<div>
+                <h2>List of Food</h2>
+                <ul>
+                    {foods.map((food, index) => <li key={index}>
+                                                         {food}
+                                                         </li>)}
+                </ul>
+                <input type="text" id="foodInput" placeholder="Enter food name"/>
+                <button onClick={handleAddFood}>Add food</button>
+           </div>)
 }
 export default MyComponent
