61abfb727f23898d94d8d914f96128fddd4a301c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-09 22:31:35 +0900

    Update Button.jsx

--- click-event/src/Button.jsx
+++ click-event/src/Button.jsx
@@ -1,20 +1,22 @@
 
 function Button(){
 
-    let count = 0;
-    const handleClick = (name) => {
-        if(count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} time/s`);
-        }
-        else{
-            console.log(`${name}, stop clicking me!`);
-        }
-    }
+
+    const handleClick = (e) => e.target.textContent = "OUCH! 😣";
+    // let count = 0;
+    // const handleClick = (name) => {
+    //     if(count < 3){
+    //         count++;
+    //         console.log(`${name} you clicked me ${count} time/s`);
+    //     }
+    //     else{
+    //         console.log(`${name}, stop clicking me!`);
+    //     }
+    // }
 
     // const handleClick2 = (name) => console.log(`${name}, stop clicking me!`)
 
-    return(<button onClick={() => handleClick("Ryan")}>Click me 😊</button>)
+    return(<button onClick={(e) => handleClick(e)}>Click me 😊</button>)
 }
 
 export default Button
