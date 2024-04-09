6e5bdc36783c7dca9c2ea2bb2a8a3a3be1d3ea2a
Ryan Dempsey <rjd9896@gmail.com>
2024-04-09 22:27:34 +0900

    Update Button.jsx

--- click-event/src/Button.jsx
+++ click-event/src/Button.jsx
@@ -3,13 +3,9 @@ function Button(){
 
     let count = 0;
     const handleClick = (name) => {
-        if(count < 3 && count < 1){
+        if(count < 3){
             count++;
-            console.log(`${name} you clicked me ${count} time`);
-        }
-        else if(count > 1 && count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} times`);
+            console.log(`${name} you clicked me ${count} time/s`);
         }
         else{
             console.log(`${name}, stop clicking me!`);
