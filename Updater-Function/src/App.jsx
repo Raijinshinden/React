9e756ab25eb6da80790f8806fe3715eb92bc02be
Ryan Dempsey <rjd9896@gmail.com>
2024-04-10 00:28:10 +0900

    Created a file to learn about the updater function

---
+++ Updater-Function/src/App.jsx
@@ -0,0 +1,12 @@
+// updater function = A function passed as an argument to setState() usually
+//                    ex. setYear(year + 1)
+//                    Allow for safe updates based on the previous state
+//                    Used with multiple state updates and asynchronous functions
+//                    Good practice to use updater functions
+
+function App() {
+
+  return(<></>)
+}
+
+export default App
