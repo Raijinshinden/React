8432cb067737c1455fa53260079ab58f96908fe1
Ryan Dempsey <rjd9896@gmail.com>
2024-04-11 00:57:09 +0900

    Update MyComponent.jsx

--- UseEffect/src/MyComponent.jsx
+++ UseEffect/src/MyComponent.jsx
@@ -20,6 +20,9 @@ import React, {useState, useEffect} from 'react';
 
 function MyComponent(){
 
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] =useState(window.innerHeight)
+
     // const [count, setCount] = useState(0);
     // const [color, setColor] = useState("green");
 
@@ -45,5 +48,30 @@ function MyComponent(){
     //    <button onClick={subtractCount}>Subtract</button><br/>
     //    <button onClick={changeColor}>Change Color</button>
     // </>)
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED")
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED");
+        }
+    }, []);
+
+    useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height]);
+
+    
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
+    return(<>
+           <p>Window Width: {width}px</p>
+           <p>Window Height: {height}px</p>
+           </>)
 }
 export default MyComponent
