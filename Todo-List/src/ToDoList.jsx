5fc9cfda2d2ae7948405417ca5084bc3e314dae2
Ryan Dempsey <rjd9896@gmail.com>
2024-04-11 00:08:16 +0900

    Update ToDoList.jsx

--- Todo-List/src/ToDoList.jsx
+++ Todo-List/src/ToDoList.jsx
@@ -27,7 +27,9 @@ function ToDoList(){
     function moveTaskUp(index){
         if(index > 0){
             const updatedTasks = [...tasks];
-            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            [updatedTasks[index], updatedTasks[index - 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
         }
     }
 
