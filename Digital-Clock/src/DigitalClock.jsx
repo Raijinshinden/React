19f9c9a3d1fd0a2411eb58d6f483a1c1f6f44c2c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-12 00:01:34 +0900

    Update DigitalClock.jsx

--- Digital-Clock/src/DigitalClock.jsx
+++ Digital-Clock/src/DigitalClock.jsx
@@ -2,6 +2,22 @@ import React, {useState, useEffect} from 'react';
 
 function DigitalClock(){
 
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+    function formatTime(){
+        let hours = time.getHours();
+    }
+
     return(
         <div className="clock-container">
             <div className="clock">
