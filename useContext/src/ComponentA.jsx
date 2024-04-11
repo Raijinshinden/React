c11b0760f6e10ca2216a43198a7cb29694824050
Ryan Dempsey <rjd9896@gmail.com>
2024-04-12 00:28:25 +0900

    updates

---
+++ useContext/src/ComponentA.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import ComponentB from "./ComponentB.jsx"
+function ComponentA(){
+
+    const [user, setUser] = useState("Raichu")
+
+    return(
+        <div className="box">
+            <h1>ComponentA</h1>
+            <h2>{`Hello ${user}`}</h2>
+            <ComponentB user={user}/>
+        </div>)
+}
+export default ComponentA
